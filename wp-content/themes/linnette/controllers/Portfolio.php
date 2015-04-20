1e73cd50ae363f8aed0013fc787e2bc7e21ebfc7
Jakub Klapka <klapka@lumiart.cz>
2015-04-20 12:19:34 +0200

    Add portfolio breadcrumbs

--- wp-content/themes/linnette/controllers/Portfolio.php
+++ wp-content/themes/linnette/controllers/Portfolio.php
@@ -19,6 +19,8 @@ class Portfolio {
 
 		$this->register_post_type();
 
+		add_action( 'wp', array( $this, 'check_for_portfolio_type' ) );
+
 	}
 
 	private function register_post_type() {
@@ -78,5 +80,62 @@ class Portfolio {
 
 	}
 
+	public function check_for_portfolio_type() {
+
+		if( is_singular( 'portfolio' ) ) {
+
+			add_filter( 'timber_context', array( $this, 'add_portfolio_breadcrumbs' ) );
+
+		}
+
+	}
+
+	public function add_portfolio_breadcrumbs( $context ) {
+		$post = new \TimberPost();
+		$terms = $post->get_terms( 'portfolio_category' );
+
+		if( empty( $terms ) ) return $context;
+
+		//Get only last term for breadcrumbs
+		$base_term = end( $terms );
+		$terms_for_breadcb = array( $base_term );
+
+		$iterating_term = $base_term;
+		while( $iterating_term->parent !== 0 ) {
+			$new_term = new \TimberTerm( $iterating_term->parent, 'portfolio_category' );
+			$terms_for_breadcb[] = $new_term;
+			$iterating_term = $new_term;
+		}
+
+		//Make it from parent to children
+		$terms_for_breadcb = array_reverse( $terms_for_breadcb );
+
+		/*
+		 * Build BC
+		 */
+		$breadcrumbs = array(
+			(object)array(
+				'title' => 'Portfolio',
+				'url' => get_post_type_archive_link( 'portfolio' )
+			)
+		);
+
+		/** @var \TimberTerm $term */
+		foreach( $terms_for_breadcb as $term ) {
+			$breadcrumbs[] = (object)array(
+				'title' => $term->name,
+				'url' => $term->get_url()
+			);
+		}
+
+		$breadcrumbs[] = (object)array(
+			'title' => $post->title(),
+			'url' => $post->permalink()
+		);
+
+		$context[ 'breadcrumbs' ] = $breadcrumbs;
+
+		return $context;
+	}
 
 }
