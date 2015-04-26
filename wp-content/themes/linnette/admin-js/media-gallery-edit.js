4fad5f507afeb61a137264a096603a439b2a3c2f
Jakub Klapka <klapka@lumiart.cz>
2015-04-26 17:05:30 +0200

    Modify post add media modal for clever defaults

--- wp-content/themes/linnette/admin-js/media-gallery-edit.js
+++ wp-content/themes/linnette/admin-js/media-gallery-edit.js
@@ -3,9 +3,37 @@
 
 	$( function() {
 
-		wp.media.editor.get().bind('open', function(){
-			console.log('test');
-		})
+		/*
+		Set create gallery as default tab
+		 */
+		$( 'body' ).watch( {
+			properties: 'attr_class',
+			callback: function( data, i ){
+				if( data.vals[ 0 ].indexOf( 'modal-open' ) !== -1 ){
+					$( '.media-menu .media-menu-item:nth-child(2)' ).trigger( 'click' );
+				}
+			}
+		} );
+
+		/*
+		Hide some options on gallery setting
+		 */
+		$( document ).arrive( '.collection-settings.gallery-settings', function() {
+
+			var settings = $( '.collection-settings.gallery-settings' );
+			settings.find( '[data-setting="link"]' ).parent().css( 'visibility', 'hidden');
+			settings.find( '[data-setting="size"]' ).parent().css( 'visibility', 'hidden');
+
+			settings.find( '[data-setting="columns"]' ).val( '1' ).trigger( 'change' );
+			var column_options = settings.find( '[data-setting="columns"] option' );
+			column_options.each( function(){
+				var option = $( this );
+				if( option.prop( 'value' ) > 2 ) {
+					option.remove();
+				}
+			} );
+
+		} );
 
 	} );
 
--- wp-content/themes/linnette/controllers/AdminModifications.php
+++ wp-content/themes/linnette/controllers/AdminModifications.php
@@ -18,12 +18,23 @@ class AdminModifications {
 
 		add_editor_style( 'assets/css/editor-style.css' );
 
-//		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_media_modifications' ) );
+		add_action( 'current_screen', array( $this, 'is_edit_page' ) );
+
+
+	}
+
+	public function is_edit_page( $screen ) {
+
+		if( $screen->base === 'post' ) {
+			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_media_modifications' ) );
+		}
 
 	}
 
 	public function enqueue_media_modifications() {
-		wp_enqueue_script( 'admin-media-modifs', get_template_directory_uri() . '/admin-js/media-gallery-edit.js', array( 'jquery' ), null, true );
+		wp_register_script( 'arrive', get_template_directory_uri() . '/admin-js/arrive.min.js', array(), null, true );
+		wp_register_script( 'watch', get_template_directory_uri() . '/admin-js/jquery-watch.min.js', array( 'jquery' ), null, true );
+		wp_enqueue_script( 'admin-media-modifs', get_template_directory_uri() . '/admin-js/media-gallery-edit.js', array( 'jquery', 'arrive', 'watch' ), null, true );
 	}
 
 }
