2266f6c3ded08e4a115172d40236f5197e91285c
Jakub Klapka <klapka@lumiart.cz>
2015-05-24 15:42:06 +0200

    Edit options in wp gallery and add tinymce view

--- wp-content/themes/linnette/admin-js/media-gallery-edit.js
+++ wp-content/themes/linnette/admin-js/media-gallery-edit.js
@@ -16,24 +16,98 @@
 		} );
 
 		/*
-		Hide some options on gallery setting
+		Edit some options on gallery setting
 		 */
-		$( document ).arrive( '.collection-settings.gallery-settings', function() {
+		//TODO: remove arrive dep
+		//TODO: click on edit button
 
-			var settings = $( '.collection-settings.gallery-settings' );
-			settings.find( '[data-setting="link"]' ).parent().css( 'visibility', 'hidden');
-			settings.find( '[data-setting="size"]' ).parent().css( 'visibility', 'hidden');
+		wp.media.galleryDefaults.columns = 1;
 
-			settings.find( '[data-setting="columns"]' ).val( '1' ).trigger( 'change' );
-			var column_options = settings.find( '[data-setting="columns"] option' );
-			column_options.each( function(){
-				var option = $( this );
-				if( option.prop( 'value' ) > 2 ) {
-					option.remove();
+		// merge default gallery settings template with yours
+		wp.media.view.Settings.Gallery = wp.media.view.Settings.Gallery.extend({
+			template: function(view){
+				return wp.media.template('lumi-gallery-settings')(view);
+			}
+		});
+
+
+		/**
+		 * Edit media view in tinymce
+		 * Code based on wp-includes/js/mce-view.js
+		 */
+		( function( window, views, $ ) {
+			var postID = $( '#post_ID' ).val() || 0,
+				media, gallery, av, embed;
+
+			media = {
+				state: [],
+
+				edit: function( text, update ) {
+					var media = wp.media[ this.type ],
+						frame = media.edit( text );
+
+					this.pausePlayers && this.pausePlayers();
+
+					_.each( this.state, function( state ) {
+						frame.state( state ).on( 'update', function( selection ) {
+							update( media.shortcode( selection ).string() );
+						} );
+					} );
+
+					frame.on( 'close', function() {
+						frame.detach();
+					} );
+
+					frame.open();
+				}
+			};
+
+			gallery = _.extend( {}, media, {
+				state: [ 'gallery-edit' ],
+				template: wp.media.template( 'lumi-editor-gallery' ),
+
+				initialize: function() {
+					var attachments = wp.media.gallery.attachments( this.shortcode, postID ),
+						attrs = this.shortcode.attrs.named,
+						self = this;
+
+					attachments.more()
+						.done( function() {
+							attachments = attachments.toJSON();
+
+							_.each( attachments, function( attachment ) {
+								if ( attachment.sizes ) {
+									if ( attrs.size && attachment.sizes[ attrs.size ] ) {
+										attachment.thumbnail = attachment.sizes[ attrs.size ];
+									} else if ( attachment.sizes.thumbnail ) {
+										attachment.thumbnail = attachment.sizes.thumbnail;
+									} else if ( attachment.sizes.full ) {
+										attachment.thumbnail = attachment.sizes.full;
+									}
+								}
+							} );
+
+							/*MY EDIT*/
+							if( attrs.columns === '1_2' || attrs.columns === '2_1' ) {
+								attrs.columns = 3;
+							}
+
+							self.render( self.template( {
+								attachments: attachments,
+								columns: attrs.columns ? parseInt( attrs.columns, 10 ) : wp.media.galleryDefaults.columns
+							} ) );
+						} )
+						.fail( function( jqXHR, textStatus ) {
+							self.setError( textStatus );
+						} );
 				}
 			} );
 
-		} );
+			views.unregister( 'gallery' );
+			views.register( 'gallery', _.extend( {}, gallery ) );
+
+		} )( window, window.wp.mce.views, window.jQuery );
+
 
 	} );
 
