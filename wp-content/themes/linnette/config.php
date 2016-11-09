98208bad2920187a6be55fcb137a16d4017da4ff
Jakub Klapka <klapka@lumiart.cz>
2016-11-09 20:45:19 +0100

    Fix svg sprite ajax syntax, add lumi SFM limit

--- wp-content/themes/linnette/config.php
+++ wp-content/themes/linnette/config.php
@@ -3,3 +3,4 @@ define( 'WPCF7_AUTOP', false );
 define( 'WPCF7_ADMIN_READ_CAPABILITY', 'manage_options' );
 define( 'WPCF7_ADMIN_READ_WRITE_CAPABILITY', 'manage_options' );
 define( 'GT_EDIT_GLANCES', 'manage_options' );
+define( 'LUMI_SFM_STORAGE_LIMIT', 2 * 1024 * 1024 * 1024 );
